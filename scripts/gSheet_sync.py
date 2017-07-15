0a45fb7c8937dfa0633b8068c79e435ea0db5edf
kay-wong <kayyenwong@gmail.com>
2017-07-15 18:09:06 +1000

    Add org if no matching org found

--- scripts/gSheet_sync.py
+++ scripts/gSheet_sync.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import sys, argparse
+import sys, argparse, requests
 
 parser = argparse.ArgumentParser(description='Update Popit DB with changes from spreadsheet')
    
@@ -43,8 +43,8 @@ if __name__ == '__main__':
     sys.argv = ['gSheet_utils']
     import gSheet_utils
     
-    df, col_AI_map = gSheet_utils.sheetChanges(args.spreadsheetId, args.spreadsheetId_control, args.sheetName, args.sheetName, args.updateType+'_id')
-    #df, col_AI_map = gSheet_utils.importGSheetAsDF(args.spreadsheetId, args.sheetName)
+    #df, col_AI_map = gSheet_utils.sheetChanges(args.spreadsheetId, args.spreadsheetId_control, args.sheetName, args.sheetName, args.updateType+'_id')
+    df, col_AI_map = gSheet_utils.importGSheetAsDF(args.spreadsheetId, args.sheetName)
     gSheet_details = {'sheetID':args.spreadsheetId, 'sheetName': args.sheetName, 'col_AI_map': col_AI_map, 'sub_langs':sub_langs}
          
    
@@ -53,10 +53,25 @@ if __name__ == '__main__':
         import searchCLI
         orgName = ' '.join(args.sheetName.split('memberships_')[1].split('_'))
         orgID = searchCLI.searchCLI(base_url, orgName, 'organizations', 'name', 'othernames')
-    
+        
+        if not orgID:
+            orgP = {'name': orgName}
+            url = base_url+ "/en/organizations/"
+            r_en = requests.post(url, headers=headers, json=orgP)
+            
+            if r_en.ok:
+                try:
+                    orgID = r_en.json()['result']['id']
+                except KeyError:
+                    orgID = r_en.json()['id']
+            else:
+                print(orgP)
+                print(r_en.content)
+        
         sys.argv = ['membershipsUpdate_langs.py']
         import membershipsUpdate_langs  
         df.apply(lambda row: membershipsUpdate_langs.genPayload(base_url, headers, row, orgID, gSheet_details, sub_langs), axis=1)
+        
           
     elif classType == 'person':
         sys.argv = ['personsUpdate_langs.py']
@@ -69,3 +84,5 @@ if __name__ == '__main__':
 
 
   
+    
+    
